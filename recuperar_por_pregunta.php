8cb1856208d74ad1fac831b7dfdeaecbd8a427a9
FMacotto <fernandomacotto89@gmail.com>
2022-10-03 14:42:15 -0600

    Commit algunos arreglos peques

--- recuperar_por_pregunta.php
+++ recuperar_por_pregunta.php
@@ -1,6 +1,6 @@
 <?php
     include('db.php');
-    $primerRespuesta=$_POST['respuesta'];
+    $primerRespuesta=strtoupper($_POST['respuesta']);
     $preguntas=$_POST['Lapregunta'];
     $contrasenas=$_POST['password'];
     $contrasenas_=$_POST['password_'];
@@ -20,13 +20,27 @@
         $Respuesta=$otra_['Respuesta'];
     }
 
-    if ($filas and $primerRespuesta==$Respuesta and $contrasenas=$contrasenas_) {
+    $consulta_Contra_="SELECT Contrasena FROM tbl_ms_usuario WHERE Id_Usuario='$Id'";
+    $Respuesta__=mysqli_query( $conexion , $consulta_Contra_ );
+    while ($otra___=mysqli_fetch_array( $Respuesta_ )) {
+        # code...
+        $Respuesta___contra=$otra___['Contrasena'];
+    }
+
+    $consulta_Usuario="SELECT Usuario FROM tbl_ms_usuario WHERE Id_Usuario='$Id'";
+    $Respuesta_Usuario=mysqli_query( $conexion , $consulta_Usuario );
+    while ($otra____=mysqli_fetch_array( $Respuesta_Usuario )) {
+        # code...
+        $Respuesta___Usuario=$otra____['Usuario'];
+    }
+
+    if (($filas==1) and $primerRespuesta==$Respuesta and $contrasenas=$contrasenas_ and $Respuesta___contra<>$contrasenas_ and $contrasenas<>$Respuesta___Usuario) {
         # code...
         $actualizarContra = "UPDATE tbl_ms_usuario SET contrasena = $contrasenas_ WHERE Id_Usuario = '$Id'";
         mysqli_query( $conexion , $actualizarContra );
         echo '<script>alert("CONTRASEÑA CAMBIADA CON EXITO");</script>';
         include('login.html');
-    }else {
+    }else{
         # code...
         echo '<script>alert("ERROR FATAL");</script>';
         include('login.html');
