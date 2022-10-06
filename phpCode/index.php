7b6c5450141197380fa83442066e8eb7bff4efd4
EnriqueUNAH <hbustillo@unah.hn>
2022-10-05 22:54:11 -0600

    asd

--- phpCode/index.php
+++ phpCode/index.php
@@ -1,10 +1,1 @@
-<html>
-       <head>
-       <title>Dashboard - INVENTARIOS DEL ATLANTICO</title>
-       </head>
-   <body> 
-        <?php
-              include('login.html');
-         ?>
-   </body>
-<html>
+<?php include('../login.html');?>
