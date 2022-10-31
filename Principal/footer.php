2d2267c9ea507b62cc97e536b91faef871af1b97
FMacotto <fernandomacotto89@gmail.com>
2022-10-31 09:07:42 -0600

    commit random

--- Principal/footer.php
+++ Principal/footer.php
@@ -1,7 +1,7 @@
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span></span></strong>. Todos los derechos resevados (Diseños Alpha)
+      &copy; Copyright <strong><span></span></strong>. Todos los derechos reservados (Diseños Alpha)
     </div>
     <div class="credits">
       <!-- All the links in the footer should remain intact. -->
