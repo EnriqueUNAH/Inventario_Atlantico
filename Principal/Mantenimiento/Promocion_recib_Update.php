d5279feb86d3cfc06712359bded6bb78b347c185
ElvinH5 <elvinh5@hotmail.com>
2022-11-12 19:58:51 -0600

    Error corregido en: Editar Promoción.

--- Principal/Mantenimiento/Promocion_recib_Update.php
+++ Principal/Mantenimiento/Promocion_recib_Update.php
@@ -4,14 +4,14 @@ session_start();
 $usuario =$_SESSION['nombre'];
 
 include('../db2.php');
-$COD_Promocion = $_POST['COD_Promocion'];
-$NOMBRE_Promocion = strtoupper($_POST[ 'NOMBRE_Promocion' ]);
+$COD_Promocion = $_POST['COD_PROMOCION'];
+$NOMBRE_Promocion = strtoupper($_POST[ 'NOMBRE_PROMOCION' ]);
 
 $update = ("UPDATE tbl_Promocion 
 	SET 
 	NOMBRE_Promocion  ='" .$NOMBRE_Promocion. "'
   
-WHERE COD_Promocion='" .$COD_Promocion. "'
+WHERE COD_PROMOCION='" .$COD_Promocion. "'
 ");
 $result_update = mysqli_query($conexion2, $update);
 
