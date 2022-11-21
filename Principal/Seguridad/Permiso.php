e6a2bf22cbc54f89ae859dde01a545248f4de6bb
ElvinH5 <elvinh5@hotmail.com>
2022-11-21 16:48:50 -0600

    Ejemplo en Permiso

--- Principal/Seguridad/Permiso.php
+++ Principal/Seguridad/Permiso.php
@@ -1,206 +1,115 @@
-<?php include('../cabecera.php') ?>
-<?php include('../sidebar.php') ?>
-
-<!DOCTYPE html>
-<html lang="es">
-	<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-<!--xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-->
-  <link type="text/css" rel="shortcut icon" href="../img/logo-mywebsite-urian-viera.svg"/>
-  <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-  <link rel="stylesheet" type="text/css" href="../css/cargando.css">
-  <link rel="stylesheet" type="text/css" href="../css/maquinawrite.css">
-
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    	<!-- SCRIPTS JS-->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<script src="peticion.js"></script>
-
-    <style> 
-        table tr th{
-            background:rgba(0, 0, 0, .6);
-            color: black;
-        }
-        tbody tr{
-          font-size: 12px !important;
+<form action="#" method="POST">
+		<?php
+			$nombre = "";
+			$password = "";
+			$email = "";
+			$pais = "";
+			$nivel = "";
+			// TODO (1) declarar variable como arreglo
+			$lenguajes = array();
+			
+			if(isset($_POST['nombre'])){
+				$nombre = $_POST['nombre'];
+				$password = $_POST['password'];
+				$email = $_POST['email'];
+				$pais = $_POST['pais'];
+				
+				if(isset($_POST['nivel'])){
+					$nivel = $_POST['nivel'];
+				}else{
+					$nivel = "";
+				}
+
+				// TODO (2) Validar que existe variable nivel
+				if(isset($_POST['lenguajes'])){
+					$lenguajes = $_POST['lenguajes'];
+				}else{
+					$lenguajes = [];
+				}
+				
+
+				$campos = array();
+
+				if($nombre == ""){
+					array_push($campos, "El campo Nombre no pude estar vacío");
+				}
+
+				if($password == "" || strlen($password) < 6){
+					array_push($campos, "El campo Password no puede estar vacío, ni tener menos de 6 caracteres.");
+				}
+
+				if($email == "" || strpos($email, "@") === false){
+					array_push($campos, "Ingresa un correo electrónico válido.");
+				}
+
+				if($pais == ""){
+					array_push($campos, "Selecciona un país de origen.");
+				}
+
+				if($nivel == ""){
+					array_push($campos, "Selecciona un nivel de desarrollo.");
+				}
+				// TODO (3) Validar que el campo no esté vacío
+				if($lenguajes == "" || count($lenguajes) < 2){
+					array_push($campos, "Selecciona al menos dos lenguajes de programación.");
+				}
+
+				if(count($campos) > 0){
+					echo "<div class='error'>";
+					for($i = 0; $i < count($campos); $i++){
+						echo "<li>".$campos[$i]."</i>";
+					}
+				}else{
+					echo "<div class='correcto'>
+							Datos correctos";
+				}
+				echo "</div>";
+			}
+		?>
+		<p>
+		Nombre:<br/>
+		<input type="text" name="nombre" value="<?php echo $nombre; ?>">
+		</p>
+
+		<p>
+		Password:<br/>
+		<input type="password" name="password" value="<?php echo $password; ?>">
+		</p>
+
+		<p>
+		correo electrónico:<br/>
+		<input type="text" name="email" value="<?php echo $email; ?>">
+		</p>
+
+		<p>
+			País de origen: <br>	
+			<select name="pais" id="">
+				<option value="">Selecciona un país</option>
+				<option value="mx" <?php if($pais == "mx") echo "selected" ?> >México</option>
+				<option value="eu" <?php if($pais == "eu") echo "selected" ?>>Estados Unidos</option>
+				<option value="es" <?php if($pais == "es") echo "selected" ?>>España</option>
+				<option value="ar" <?php if($pais == "ar") echo "selected" ?>>argentina</option>
+				<option value="ch" <?php if($pais == "ch") echo "selected" ?>>China</option>
+			</select>
+		</p>
+
+		<p>
+			Nivel de desarrollo <br>	
+			<input type="radio" name="nivel" value="principiante" <?php if($nivel == "principiante") echo "checked"; ?>> Principiante
+			<input type="radio" name="nivel" value="intermedio" <?php if($nivel == "intermedio") echo "checked"; ?>> Intermedio
+			<input type="radio" name="nivel" value="avanzado" <?php if($nivel == "avanzado") echo "checked"; ?>> Avanzado
+		</p>
 
-        }
-        h3{
-            color:crimson; 
-            margin-top: 100px;
-        }
-        a:hover{
-            cursor: pointer;
-            color: #333 !important;
-        }
-      </style>
+		<p>
+			Lenguajes de programación <br>	
+			<input type="checkbox" name="lenguajes[]" value="php" <?php if(in_array("php", $lenguajes)) echo "checked"; ?>> PHP <br>
+			<input type="checkbox" name="lenguajes[]" value="js" <?php if(in_array("js", $lenguajes)) echo "checked"; ?>> Javascript <br>
+			<input type="checkbox" name="lenguajes[]" value="java" <?php if(in_array("java", $lenguajes)) echo "checked"; ?>> Java <br>
+			<input type="checkbox" name="lenguajes[]" value="swift" <?php if(in_array("swift", $lenguajes)) echo "checked"; ?>> Swift <br>
+			<input type="checkbox" name="lenguajes[]" value="py" <?php if(in_array("py", $lenguajes)) echo "checked"; ?>> Python <br>
+		</p>
 
-      
-	</head>
-  
-	<body>
-    <main id="main" class="main">
-    <?php
-        include('../db2.php');
 
-        $sqlCliente   = ("SELECT * FROM  tbl_ms_roles as rol inner join tbl_ms_permisos as permiso on permiso.ID_ROL = rol.ID_ROL
-                                                                    inner join tbl_ms_objetos as objeto on objeto.ID_OBJETO = permiso.ID_OBJETO");
-        $queryCliente = mysqli_query($conexion2, $sqlCliente);
-        $cantidad     = mysqli_num_rows($queryCliente);
-    ?>
 
-<div class="row text-center" style="background-color: #cecece">
-</div>
-
-
-
-
-
-<div class="row clearfix">
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-  <div class="body">
-      <div class="row clearfix">
-
-      <div class="col-sm-12"><h2><b>PERMISOS</b></h2></div>
-            <p></p>
-
-   
-         
-
-          <div class="col-sm-20">
-              <div class="row">
-                <div class="col-md-12 p-2">
-
-
-                <div class="table-responsive">
-                    <table class="table table-bordered table-striped table-hover">
-                        <thead>
-                          <tr>
-                            <th> ROL</th>
-                            <th> PANTALLA</th>
-                            <th> PERMISO INSERCIÓN</th>
-                            <th> PERMISO ELIMINACIÓN</th>
-                            <th> PERMISO ACTUALIZACIÓN</th>
-                            <th> PERMISO_CONSULTAR</th>
-                     
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <?php
-                              while ($dataCliente = mysqli_fetch_array($queryCliente)) { ?>
-                          <tr>
-                          <td><?php echo $dataCliente['ROL']; ?></td>
-                          <td><?php echo $dataCliente['OBJETO']; ?></td>
-            <input type="checkbox" name="lenguajes[]" value="insertar" <?php if(mysqli_fetch_array("PERMISO_INSERCION", $queryCliente)) echo "checked"; ?>> Insertar <br>
-			<input type="checkbox" name="lenguajes[]" value="eliminar" <?php if(mysqli_fetch_array("PERMISO_ELIMINAR", $queryCliente)) echo "checked"; ?>> Eliminar <br>
-			<input type="checkbox" name="lenguajes[]" value="actualizar" <?php if(mysqli_fetch_array("PERMISO_ACTUALIZAR", $queryCliente)) echo "checked"; ?>> Actualizacion <br>
-			<input type="checkbox" name="lenguajes[]" value="consultar" <?php if(mysqli_fetch_array("PERMISO_CONSULTAR", $queryCliente)) echo "checked"; ?>> Consultar <br>
-		
-                          </tr>
-                                                               
-                         
-                            
-
-
-                        <?php } ?>
-                        <input type="submit" value="GUARDAR PERMISOS">
-
-                    </table>
-
-
-
-
-
-
-
-
-
-
-
-
-
-                </div>
-
-
-              </div>
-          </div>
-          </div>
-      </div>
-  </div>
-</div>
-
-<script src="../js/jquery.min.js"></script>
-<script src="../js/popper.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>
-
-
-
-
-
-
-<script type="text/javascript">
-    $(document).ready(function() {
-
-        $(window).load(function() {
-            $(".cargando").fadeOut(1000);
-        });
-
-//Ocultar mensaje
-    setTimeout(function () {
-        $("#contenMsjs").fadeOut(1000);
-    }, 3000);
-
-
-
-    $('.btnBorrar').click(function(e){
-        e.preventDefault();
-        var id = $(this).attr("id");
-
-        var dataString = 'ID_ROL='+ id;
-        url = "Objetos_recib_Delete.php";
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: dataString,
-                success: function(data)
-                {
-                  window.location.href="Permisos.php";
-                  $('#respuesta').html(data);
-                }
-            });
-    return false;
-
-    });
-
-
-});
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
-</script>
-
-    </main>
-    
-	</body>
-</html>
-
-<?php include("../footer.php")?>
+		<p><input type="submit" value="enviar datos"></p> 
+	</form>
